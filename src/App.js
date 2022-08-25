60e0e6b7633881b0fba2af0dda48bcf0df78c7b5
jmarcossf <jamlmarcos@gmail.com>
2022-08-24 17:53:29 -0700

    Reformatted App.js with prettier.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export const replaceCamelWithSpaces =(colorName) => {
-    return colorName.replace(/\B([A-Z])\B/g, ' $1');
-}
+export const replaceCamelWithSpaces = colorName => {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+};
 
 function App() {
-    const [buttonColor, setButtonColor] = useState('MediumVioletRed');
-    const [buttonIsEnabled, setButtonIsEnabled] = useState(true);
-    const newButtonColor = buttonColor === 'MediumVioletRed' ? 'blue' : 'MediumVioletRed';
+  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
+  const [buttonIsEnabled, setButtonIsEnabled] = useState(true);
+  const newButtonColor = buttonColor === "MediumVioletRed" ? "blue" : "MediumVioletRed";
 
-    const toggleColorButtonCheckbox = () => {
-        setButtonIsEnabled(!buttonIsEnabled);
-        setButtonColor('gray');
-    }
+  const toggleColorButtonCheckbox = () => {
+    setButtonIsEnabled(!buttonIsEnabled);
+    setButtonColor("gray");
+  };
 
-    return (
-        <div>
-            <button
-                style={{ backgroundColor: !buttonIsEnabled ? 'gray' : buttonColor }}
-                onClick={() => setButtonColor(newButtonColor)}
-                disabled={!buttonIsEnabled}
-            >
-                Change to {newButtonColor}
-            </button>
-            <br />
-            <input type="checkbox" onChange={toggleColorButtonCheckbox} id="disable-button-checkbox" />
-            <label htmlFor="disable-button-checkbox" >Disable button</label>
-        </div>
-    );
+  return (
+    <div>
+      <button
+        style={{ backgroundColor: !buttonIsEnabled ? "gray" : buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={!buttonIsEnabled}
+      >
+        Change to {newButtonColor}
+      </button>
+      <br />
+      <input type="checkbox" onChange={toggleColorButtonCheckbox} id="disable-button-checkbox" />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
+    </div>
+  );
 }
 
 export default App;
